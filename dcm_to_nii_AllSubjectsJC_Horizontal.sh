8f3ed783afd6aa7a5bfc2ba9891ebe6bbfdb00b1
Jeiran Choupan <jayran.chupan@gmail.com>
2020-10-23 12:27:58 -0700

    Update dcm_to_nii_AllSubjectsJC_Horizontal.sh
    adjusted comments

--- dcm_to_nii_AllSubjectsJC_Horizontal.sh
+++ dcm_to_nii_AllSubjectsJC_Horizontal.sh
@@ -12,17 +12,17 @@
 	DICOM_FOLDER=$3 					# "NAME_OF_THE_DICOM_FOLDER"
 	OUTPUT_FOLDER=$4  					# "PATH_TO_Subject's_OUTPUT_NIFTI_FOLDER"
 	Study_folder=$5 					# "PATH_TO_STUDY_FOLDER"
-	PATH_TO_NIfTI_20140122_FOLDER=$6	# "Path to Nifti Matlab toolbox"
+	PATH_TO_NIfTI_20140122_FOLDER=$6			# "Path to Nifti Matlab toolbox"
 	
 
 	for p in ${OUTPUT_FOLDER}/SubjectsID
 
 	do
-		i=`echo $p | rev | cut -d '/' -f1 | rev`  # `i` is the subject name - Adjust `-f12` based on the folder subdirectories
+		i=`echo $p | rev | cut -d '/' -f1 | rev`  # `i` is the subject name 
 
 		for l in ${p}/OD_or_OS_Folders
 		do
-			j=`echo $l | rev | cut -d '/' -f1 | rev` # `j` is the name of the OD and OS folders - Adjust `-f13` based on the folder subdirectories
+			j=`echo $l | rev | cut -d '/' -f1 | rev` # `j` is the name of the OD and OS folders 
 
 			for g in ${INPUT_DIR}/HCLV${i}_OD_OS_DICOM/${j}/Volume_scans/${DICOM_FOLDER}
 			
@@ -32,18 +32,19 @@
 
 				dcm2niix -f oct -o ${OUTPUT_FOLDER}/${i}/${j} ${g}/  # Convert Horizontal scans dicom file to nifti. The resulting nifti 
 				# orientation is {-1 0 0;
-				#				   0 1 0;
-				#		  	 	   0 0 1}
+				#		  0 1 0;
+				#		  0 0 1}
 
 				# Next, use the QIT software to standardize the converted nifti file orientation
 				# into RAS {1 0 0;
-				#			0 1 0;
-				#			0 0 1}
+				#	    0 1 0;
+				#	    0 0 1}
 
 				qit VolumeStandardize --input ${OUTPUT_FOLDER}/${i}/${j}/oct.nii --output ${OUTPUT_FOLDER}/${i}/${j}/oct_std.nii 
 				
 				# Then, use Matlab in the terminal and loads the standardized nifti OCT file
 				# The following adjustments to the header of the nifti file changes the placements of pixel dimensions 
+				
 				matlab -nodisplay -nojvm -nosplash -r "addpath(genpath('${PATH_TO_NIfTI_20140122_FOLDER}'));\
 				I = load_untouch_nii('${OUTPUT_FOLDER}/${i}/${j}/oct_std.nii');\
 				px2 = I.hdr.dime.pixdim(2);\
@@ -56,7 +57,8 @@
 				I.hdr.hist.srow_y(2) = dim1;\
 				save_untouch_nii(I,'${OUTPUT_FOLDER}/${i}/${j}/oct_std_horizontal.nii');\
 				exit"
-				# In the resulting Vertical scan the Optical Nerve Head is in the right side od fovea for OD (Left side of nifti file), and in the left side of fovea for OS (Right side of nifti file)
+				# In the resulting Vertical scan the Optical Nerve Head is in the right side od fovea for OD (Left side of nifti file), 
+				# and in the left side of fovea for OS (Right side of nifti file)
 
 				rm ${OUTPUT_FOLDER}/${i}/${j}/oct.nii
 				rm ${OUTPUT_FOLDER}/${i}/${j}/oct_std.nii
